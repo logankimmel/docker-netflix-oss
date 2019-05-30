ecdf2a7812da2573961de674130347a126cf6e7f
Logan Kimmel <logan.kimmel@docker.com>
2019-05-30 12:09:16 -0500

    Init

---
+++ eureka-client/src/main/java/hello/EurekaClientApplication.java
@@ -0,0 +1,30 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello from: " + env.getProperty("spring.application.name") + " on hostname: " + System.getenv("C_NAME");
+    }
+}
